19bfd672c07c2fcd220ce2944144f243d6ce9de6
Rafael Gutiérrez <ragutimar@gmail.com>
2017-01-25 09:50:51 +0100

    Add bin folder.

---
+++ bin/inc/table2class.php
@@ -0,0 +1,93 @@
+<?php
+
+/*  Copyright 2009-2011 Rafael Gutierrez Martinez
+ *  Copyright 2012-2013 Welma WEB MKT LABS, S.L.
+ *  Copyright 2014-2016 Where Ideas Simply Come True, S.L.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * @author Rafael Gutierrez <rgutierrez@wiscot.com>
+ * @version 3.0.0 Surface
+ */
+
+use nabu\core\CNabuEngine;
+
+require_once 'common.php';
+require_once 'cli.php';
+require_once 'db2class.php';
+
+$nb_engine = CNabuEngine::getEngine();
+$mysql_connector = $nb_engine->getMainDB();
+
+if (!$mysql_connector->isConnected()) {
+    die ("Database not found or not connected\n");
+}
+
+$creator_name = nbCLICheckOption('cn', 'creator-name', ':', null, false);
+if ($creator_name === null) {
+    $creator_name = nbCLIInput('Creator name', false);
+}
+
+$creator_email = nbCLICheckOption('cm', 'creator-email', ':', null, false);
+if ($creator_email === null) {
+    $creator_email = nbCLIInput('Creator email', false);
+}
+
+$table_schema = nbCLICheckOption('s', 'schema', ':', null, false);
+if ($table_schema === null) {
+    $table_schema = nbCLIInput('Schema', false);
+}
+
+$table_name = nbCLICheckOption('t', 'table', ':', null, false);
+if ($table_name === null) {
+    $table_name = nbCLIInput('Table name', false);
+}
+
+$class_ns = nbCLICheckOption('ns', 'namespace', ':', null, false);
+if ($class_ns === null) {
+    $class_ns = nbCLIInput('Namespace', true);
+}
+
+$entity_name = nbCLICheckOption('en', 'entity-name', ':', null, false);
+if ($entity_name === null) {
+    $entity_name = nbCLIInput('Entity name', tableNameToEntity($table_name));
+}
+
+$class_name = nbCLICheckOption('c', 'class', ':', null, false);
+if ($class_name === null) {
+    $class_name = nbCLIInput('Class name', tableNameToClass($table_name));
+}
+
+$class_path = nbCLICheckOption('t', 'path', ':', null, false);
+if ($class_path === null) {
+    $class_path = nbCLIInput('Base path: ', false);
+}
+
+$class_filename = createFilename($class_path, $class_ns, $class_name);
+
+echo "\nWe go to create a new class file from a table descriptor.\n";
+echo "Before start we go to validate all fields.\n";
+echo "\nYour credentials:\n";
+echo "\n    $creator_name <$creator_email>\n";
+echo "\nYou want to create the class:\n";
+echo "\n    $class_ns\\$class_name\n";
+echo "\nStored in:\n";
+echo "\n    $class_filename.php\n";
+echo "\nFrom table:\n";
+echo "\n    " . $table_schema . '.' . $table_name . "\n\n";
+
+nbCLICheckContinue();
+
+createClassFile($table_schema, $table_name, $class_ns, $class_name, $entity_name, $class_filename);
---
+++ bin/nabu-schema2class.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+echo ========================================================================
+echo Nabu 3 -  Export Schema
+echo ========================================================================
+echo Copyright 2009-2011 Rafael Gutierrez Martinez
+echo Copyright 2012-2013 Welma WEB MKT LABS, S.L.
+echo Copyright 2014-2016 Where Ideas Simply Come True, S.L.
+echo 
+echo Licensed under the Apache License, Version 2.0 \(the "License"\);
+echo you may not use this file except in compliance with the License.
+echo You may obtain a copy of the License at
+echo
+echo     http://www.apache.org/licenses/LICENSE-2.0
+echo
+echo Unless required by applicable law or agreed to in writing, software
+echo distributed under the License is distributed on an \"AS IS\" BASIS,
+echo WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+echo See the License for the specific language governing permissions and
+echo limitations under the License.
+echo ========================================================================
+echo
+
+# This variable defines the path for config files. You can change this value.
+# When the PHP install script runs, he creates this path if not exists.
+NABU_ETC_PATH=/etc/opt/nabu-3.conf.d
+
+if [ -d ${NABU_ETC_PATH} ] && [ -f ${NABU_ETC_PATH}/nabu-3.conf ] ; then
+    source ${NABU_ETC_PATH}/nabu-3.conf
+else
+    echo Config file not found
+    exit 1
+fi
+
+if [ -f ${NABU_BIN_PATH}/inc/schema2class.php ] ; then
+    php -d safe_mode=Off -d open_basedir=none -d include_path=.:${NABU_BASE_PATH}/phputils/ ${NABU_BIN_PATH}/inc/schema2class.php "$@"
+else
+    echo Execution error: schema2class.php script not found.
+fi
