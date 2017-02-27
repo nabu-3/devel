a2d1b20cab1546056de0976fb34528e654c51aaa
Rafael Gutiérrez <ragutimar@gmail.com>
2017-02-27 09:48:49 +0100

    Change PHP script call

--- bin/nabu-schema2class.sh
+++ bin/nabu-schema2class.sh
@@ -5,7 +5,7 @@ echo ========================================================================
 echo Copyright 2009-2011 Rafael Gutierrez Martinez
 echo Copyright 2012-2013 Welma WEB MKT LABS, S.L.
 echo Copyright 2014-2016 Where Ideas Simply Come True, S.L.
-echo 
+echo
 echo Licensed under the Apache License, Version 2.0 \(the "License"\);
 echo you may not use this file except in compliance with the License.
 echo You may obtain a copy of the License at
@@ -21,8 +21,10 @@ echo ========================================================================
 echo
 
 # This variable defines the path for config files. You can change this value.
-# When the PHP install script runs, he creates this path if not exists.
+# When nabu-3 install script runs, he creates this path if not exists.
 NABU_ETC_PATH=/etc/opt/nabu-3.conf.d
+INSTALL_PATH=`realpath $0`
+INSTALL_PATH=`dirname $INSTALL_PATH`
 
 if [ -d ${NABU_ETC_PATH} ] && [ -f ${NABU_ETC_PATH}/nabu-3.conf ] ; then
     source ${NABU_ETC_PATH}/nabu-3.conf
@@ -31,8 +33,8 @@ else
     exit 1
 fi
 
-if [ -f ${NABU_BIN_PATH}/inc/schema2class.php ] ; then
-    php -d safe_mode=Off -d open_basedir=none -d include_path=.:${NABU_BASE_PATH}/phputils/ ${NABU_BIN_PATH}/inc/schema2class.php "$@"
+if [ -f ${INSTALL_PATH}/inc/schema2class.php ] ; then
+    php ${PHP_PARAMS} ${INSTALL_PATH}/inc/schema2class.php "$@"
 else
     echo Execution error: schema2class.php script not found.
 fi
